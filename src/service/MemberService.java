959d213010ed7c007b350ad13c23ec5f54ef010f
one <songha12345678@gamil.com>
2024-09-27 18:33:28 +0900

    맴버 서비스 로그인 기능추가.

--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 import vo.*;
 
 public class MemberService {
+	public String adminId = "admin1";
+	public String adminPwd = "adminP1";
+//	private static final List<Member> aaa = null;
 
-	
-	private static final List<Member> aaa = null;
 
-
-	private List<Member> members = new ArrayList<>();
+//	private List<Member> members = new ArrayList<>();
 	
 	
-	int input = next("1.관리자 모드 , 2. 일반 모드");
-	
-	private int next (String member) {
-	Member.add(new Member("uesr1", "pass1",1));
-	Member.add(new Member("user2", "pass2",2));
-	return input;
-	}
+//	private int next (String member) {
+//	Member.add(new Member("uesr1", "pass1",1));
+//	return 0;
+//	}
 
 
 	void checkName(String id, String pwd) {
@@ -31,7 +28,7 @@ public class MemberService {
 	
 	Scanner scanner = new Scanner(System.in);
 	String menu = scanner.nextLine();
-	if (str2.length()< 1 || str1.length()> 10) {
+	if (str1.length()< 1 || str1.length()> 10) {
 		System.out.println("아이디를 1~10글자 사이로 입력하세요.");
 		
 	if (str2.length()< 1 || str2.length()> 10) {
@@ -39,38 +36,37 @@ public class MemberService {
 				}	
 			} 	
 	}
-	public void remove() {
-		Member m = findByno(next("아이디 입력"));
-		if(m == null) {
-			System.out.println("입력한 아이디는 존재하지 않습니다.");
-			return;
-		}
-	}
+//	public void remove() {
+//		Member m = findByno(next("아이디 입력"));
+//		if(m == null) {
+//			System.out.println("입력한 아이디는 존재하지 않습니다.");
+//			return;
+//		}
+//	}
 	
-	public void list() {
-		int input = next("1. 회원번호입니다. 주문을 해주세요. 2. 다시 처음으로 돌아가기.");
-		List<Member> tmp = null;
-		switch (input) {
-		case 1:
-			System.out.println("메뉴판 호출");
-			tmp = null;
-			break;
-		case 2:
-			System.out.println("다른 시스템을 입력해주세요.");
-			tmp = null;
-			break;
-		case 3:
-			System.out.println("프로그램을 종료합니다.");
-			break;
-		}
+//	public void MeanMember() {
+//		int input = next("1. 회원번호입니다. 주문을 해주세요. 2. 다시 처음으로 돌아가기.");
+//		List<Member> tmp = null;
+//		switch (input) {
+//		case 1:
+//			System.out.println("메뉴판 호출");
+//			tmp = null;
+//			break;
+//		case 2:
+//			System.out.println("다른 시스템을 입력해주세요.");
+//			tmp = null;
+//			break;
+//		case 3:
+//			System.out.println("프로그램을 종료합니다.");
+//			break;
+//		}
 	}
 
 
-	private Member findByno(int next) {
-		return null;
-	}
+//	private Member findByno(int next) {
+//		return null;
+//	}
 				
 		
-			}
 		
 	
