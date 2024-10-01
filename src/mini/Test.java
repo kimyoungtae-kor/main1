a56ed87edf6e39112c3e1f6652f9abe134d6a8af
jungyoonn <sophia7625@naver.com>
2024-10-01 20:11:33 +0900

    테스트용 메인 수정

--- src/mini/Test.java
+++ src/mini/Test.java
@@ -5,32 +5,36 @@ import common.*;
 
 public class Test {
     public static void main(String[] args) {
-        ProductService ps = new ProductService();
-        OrderService os = new OrderService();
+        ProductService productService = new ProductService();
+        OrderService orderService = new OrderService();
+        PayService payService = new PayService();
 
-        ps.getAllList();
+        productService.getAllList();
         int in = 0;
         while(true) {
-            in = Utils.next("1. 카테고리별 조회 2. 장바구니 보기 3. 장바구니 담기 4. 삭제  5. 주문하기 6. 종료"
-                    , Integer.class, i -> i < 7 && i > 0, "잘못된 입력");
+            in = Utils.next("1. 카테고리별 조회 2. 장바구니 보기 3. 장바구니 담기 4. 삭제  5. 주문하기  6. 결제 내역 보기 7. 종료"
+                    , Integer.class, i -> i < 8 && i > 0, "잘못된 입력");
             switch (in) {
                 case 1:
-                    ps.getList();
+                    productService.getList();
                     break;
                 case 2:
-                    os.printBag();
+                    orderService.printBag();
                     break;
                 case 3:
-                    ps.getAllList();
-                    os.pickMenu();
+                    productService.getAllList();
+                    orderService.pickMenu();
                     break;
                 case 4:
-                    os.removeProduct();
+                    orderService.removeProduct();
                     break;
                 case 5:
-                    os.setOrder();
+                    orderService.setOrder();
                     break;
                 case 6:
+                    payService.printOrderList();
+                    break;
+                case 7:
                     System.out.println("종료합니다");
                     return;
             }
