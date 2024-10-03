84b5ccf20574cfbe43fdc50d3b85737dc426c7e0
sophi <sophia7625@naver.com>
2024-10-03 16:24:08 +0900

    클래스 이름 리팩터링

--- src/vo/Order.java
+++ src/vo/Order.java
@@ -6,35 +6,27 @@ import java.util.List;
 /**
  *  주문 내역 관리 vo
  */
-public class OrderList {
+public class Order {
     /**
      *  주문 상품 리스트
      */
-    private List<Product> saveOrder;
+    private final List<Product> saveOrder;
 
     /**
      *  주문 번호
      */
-    private String orderNum;
+    private final String orderNum;
 
     /**
      * 주문 금액
      */
-    private int totalPrice;
+    private final int totalPrice;
     private final NumberFormat format = NumberFormat.getNumberInstance();
 
-    public OrderList(List<Product> saveOrder, String orderNum, int totalPrice) {
+    public Order(List<Product> saveOrder, String orderNum, int totalPrice) {
         this.saveOrder = saveOrder;
         this.orderNum = orderNum;
         this.totalPrice = totalPrice;
-    }
-
-    public List<Product> getSaveOrder() {
-        return saveOrder;
-    }
-
-    public void setSaveOrder(List<Product> saveOrder) {
-        this.saveOrder = saveOrder;
     }
 
     public String getOrderNum() {
@@ -49,6 +41,6 @@ public class OrderList {
     public String toString() {
         return "[ 주문 번호 :: " + orderNum +
                 ", 주문 상품 :: " + saveOrder +
-                ", 주문 금액 :: " + format.format(totalPrice) + "원 ]";
+                ", 주문 금액 :: " + format.format(totalPrice) + "원 ]\n";
     }
 }
