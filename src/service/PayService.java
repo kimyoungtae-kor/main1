236404d43011e24b09367dada7399a4a635a302f
jungyoonn <sophia7625@naver.com>
2024-10-04 16:43:43 +0900

    싱글톤 패턴 수정, 변수 이름 리팩토링

--- src/service/PayService.java
+++ src/service/PayService.java
@@ -15,11 +15,10 @@ public class PayService {
     private final ProductService productService = ProductService.getinstance();
     private final NumberFormat format = NumberFormat.getNumberInstance();
     private static final PayService payService = new PayService();
-//    private final OrderService orderService = new OrderService();
     private final OrderService orderService = OrderService.getInstance();
 
     {
-        System.out.println(" :: service :: " +orderService);
+        System.out.println(" :: payService :: " +orderService);
     }
 
     /**
@@ -33,17 +32,18 @@ public class PayService {
     /**
      *  결제 진행
      * @param total - 최종 결제 금액
+     * @param bag - 장바구니 리스트
      */
-    public void pay(int total, List<Product> plist) {
+    public void pay(int total, List<Product> bag) {
         int pay = Utils.next("1. 분할 결제  2. 일괄 결제  3. 결제 취소"
                 , Integer.class, i -> i > 0 && i < 4, "입력 오류입니다 다시 시도해 주세요");
         if(pay == 1) {
             int cnt = Utils.next("인원 수를 입력해 주세요"
                     , Integer.class, i -> i > 1, "2명 이상의 인원 수를 입력해 주세요");
             System.out.println("1인당 결제할 금액은 " + format.format(total / cnt) + "원입니다");
-            paySuccess(plist);
+            paySuccess(bag);
         } else if(pay == 2) {
-            paySuccess(plist);
+            paySuccess(bag);
         } else {
             System.out.println("! 결제 취소 ! 메뉴판으로 돌아갑니다");
             productService.getAllList();
@@ -52,28 +52,38 @@ public class PayService {
 
     /**
      * 결제 완료 및 내역 저장
+     * @param bag - 장바구니 리스트
      */
-    public void paySuccess(List<Product> pList) {
-        setOrderLists(pList);
-        Order order = orders.getLast();
+    public void paySuccess(List<Product> bag) {
+        List<Product> buyList = new ArrayList<>(bag);
+
+        setOrderLists(buyList);
+        Order order = orders.get(orders.size()-1);
 
         System.out.println("총 " + format.format(order.getTotalPrice())
                 + "원 결제가 완료되었습니다 :: 주문 번호 [" + order.getOrderNum() + "]");
-        orderService.cleanBag(pList);
+        orderService.cleanBag(bag);
     }
 
     /**
      *  주문 내역 추가 및 관리 리스트
-     * @param o - 장바구니에 담겨서 결제로 넘어온 리스트
+     * @param bag - 장바구니에 담겨서 결제로 넘어온 리스트
      */
-    public void setOrderLists(List<Product> o) {
-        String str = String.valueOf(o.size());
+    public void setOrderLists(List<Product> bag) {
+        List<Product> buyList = new ArrayList<>(bag);
+
+        String str = String.valueOf(buyList.size());
         int total = 0;
-        for(Product p : o) {
+        for(Product p : buyList) {
             str += p.getProductId();
             total += p.getPrice() * p.getAmount();
         }
-        orders.add(new Order(o, str, total));
+        orders.add(new Order(buyList, str, total));
+        for(Order order : orders) {
+            for(Product p : order.getSaveOrder()) {
+                System.out.println(p.getAmount() + " " + p.getProductId());
+            }
+        }
     }
 
     /**
@@ -81,10 +91,15 @@ public class PayService {
      */
     public void printOrderList() {
         int total = 0;
+
         System.out.println("========== 결제 내역 ==========");
-        for(Order o : orders) {
-            System.out.println(o);
-            total += o.getTotalPrice();
+        for(Order order : orders) {
+            System.out.println(order);
+            for(Product p : order.getSaveOrder()) {
+                System.out.println(" 주문 상품 :: " + p.getProductName() + " " + p.getAmount() + "개 ]");
+            }
+            System.out.println();
+            total += order.getTotalPrice();
         }
         System.out.println("============================");
         System.out.println("총 매출 : " + format.format(total) + "원");
